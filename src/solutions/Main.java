5e118d180dd13a80279d62d451b1b65143646a68
Scott Carpenter <carpenter.scott22@gmail.com>
2022-06-13 12:51:13 -0500

    added romanToInt function

--- src/solutions/Main.java
+++ src/solutions/Main.java
@@ -14,6 +14,8 @@ public class Main {
         System.out.println(Arrays.toString(twoSums(array, 10)));
         int palindrome1 = 1221;
         System.out.println(isPalindrome(palindrome1));
+
+        System.out.println(romanToInt("XXIV"));
     }
 
     public static int[] twoSums(int[] nums, int target) {
@@ -44,4 +46,56 @@ public class Main {
 
     }
 
+    public static int romanToInt(String romanNumeral) {
+        // input: a string
+        // convert the values to UPPERCASE because .includes is case-sensitive
+        romanNumeral = romanNumeral.toUpperCase();
+
+        // define a variable to add values to
+        int numericalResult = 0;
+        if (romanNumeral.contains("IV") || romanNumeral.contains("IX")) {
+            numericalResult -= 2;
+        }
+        if (romanNumeral.contains("XL") || romanNumeral.contains("XC")) {
+            numericalResult -= 20;
+        }
+        if (romanNumeral.contains("CD") || romanNumeral.contains("CM")) {
+            numericalResult -= 200;
+        }
+
+        // convert the string to a charArray
+        char[] romanNumeralArray = romanNumeral.toCharArray();
+        // loop over the string
+        for (int i = 0; i < romanNumeralArray.length; i++) {
+            // create a switch with numerals corresponding to specific values
+//            System.out.println(romanNumeral.charAt(i));
+            switch (romanNumeral.charAt(i)) {
+                case 'I':
+                    // add those values to the return variable
+                    numericalResult += 1;
+                    break;
+                case 'V':
+                    numericalResult += 5;
+                    break;
+                case 'X':
+                    numericalResult += 10;
+                    break;
+                case 'L':
+                    numericalResult += 50;
+                    break;
+                case 'C':
+                    numericalResult += 100;
+                    break;
+                case 'D':
+                    numericalResult += 500;
+                    break;
+                case 'M':
+                    numericalResult += 1000;
+                    break;
+            }
+        }
+
+        // output: returns a numerical value
+        return numericalResult;
+    }
 }
