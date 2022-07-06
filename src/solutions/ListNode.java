11843a80f0b12d8ebe559a00b6146858b8bd6ef5
Scott Carpenter <carpenter.scott22@gmail.com>
2022-07-06 13:13:22 -0500

    studying linked lists

---
+++ src/solutions/ListNode.java
@@ -0,0 +1,48 @@
+package solutions;
+
+import javax.swing.tree.TreeNode;
+import java.util.Enumeration;
+
+public class ListNode implements TreeNode {
+
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+    @Override
+    public TreeNode getChildAt(int childIndex) {
+        return null;
+    }
+
+    @Override
+    public int getChildCount() {
+        return 0;
+    }
+
+    @Override
+    public TreeNode getParent() {
+        return null;
+    }
+
+    @Override
+    public int getIndex(TreeNode node) {
+        return 0;
+    }
+
+    @Override
+    public boolean getAllowsChildren() {
+        return false;
+    }
+
+    @Override
+    public boolean isLeaf() {
+        return false;
+    }
+
+    @Override
+    public Enumeration<? extends TreeNode> children() {
+        return null;
+    }
+}
